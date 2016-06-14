401d8e694b48517e7f8e72f985ac794aa8b3875f
landlessness <brian@landlessness.net>
2016-06-14 11:57:53 -0400

    added odometer, lat, lng others. bumped version

--- index.js
+++ index.js
@@ -10,9 +10,29 @@ var Automobile = module.exports = function(opts) {
   Device.call(this);
   this.opts = opts || {};
 
+  this._lastState = 'park';
+
+  this.acceleratorPedalPosition = 0.0;
+  this.engineSpeed = 0.0;
+  this._engineSpeedHigh = 8000.0;
+  this.fuelLevel = 1.0;
+  this.fuelConsumedSinceRestart = 0.0;
+  
+  this._nissanLatitude = 42.494132;
+  this._nissanLongitude = -83.426378;
+  this._slowsLatitude = 42.331859;
+  this._slowsLongitude = -83.076048;
+  this._latDiff = this._nissanLatitude - this._slowsLatitude;
+  this._lngDiff = this._nissanLongitude - this._slowsLongitude;
+
+  this.latitude = this._nissanLatitude;
+  this.longitude = this._nissanLongitude;
+
+  this.odometer = 1000;
+
   this.vehicleSpeed = 0;
   this._vehicleSpeedIncrement = 2;
-  this._vehicleSpeedHigh = 321;
+  this._vehicleSpeedHigh = 321.0;
   this._vehicleSpeedTimeOut = null;
   this._vehicleSpeedCounter = 0;
 
@@ -37,21 +57,25 @@ Automobile.prototype.init = function(config) {
     .when('eighth', {allow: ['make-not-ready']})
     .map('make-not-ready', this.makeNotReady)
     .map('make-ready', this.makeReady)
-    .monitor('vehicleSpeed');
+    .monitor('acceleratorPedalPosition')
+    .monitor('engineSpeed')
+    .monitor('fuelLevel')
+    .monitor('fuelConsumedSinceRestart')
+    .monitor('latitude')
+    .monitor('longitude')
+    .monitor('odometer')
+    .monitor('vehicleSpeed')
 };
 
 Automobile.prototype.makeReady = function(cb) {
-  this.state = 'first';
-  this.vehicleSpeed = 0;
-  this._vehicleSpeedCounter = 0;
+  this.state = this._lastState;
   this._startMockData(cb);
   cb();
 }
 
 Automobile.prototype.makeNotReady = function(cb) {
+  this._lastState = this.state;
   this.state = 'park'
-  this.vehicleSpeed = 0;
-  this._vehicleSpeedCounter = 0;
   this._stopMockData(cb);
   cb();
 }
@@ -62,6 +86,13 @@ Automobile.prototype._startMockData = function(cb) {
     self.vehicleSpeed = (Math.sin(degToRad(self._vehicleSpeedCounter)) + 1.0) * self._vehicleSpeedHigh / 2;
     self.state = self._stateFromVehicleSpeed(self.vehicleSpeed);
     self._vehicleSpeedCounter += self._vehicleSpeedIncrement;
+    self.acceleratorPedalPosition = self.vehicleSpeed / self._vehicleSpeedHigh;
+    self.engineSpeed = self.acceleratorPedalPosition * self._engineSpeedHigh;
+    self.fuelConsumedSinceRestart += 0.01 * self.acceleratorPedalPosition;
+    self.fuelLevel = 100 - self.fuelConsumedSinceRestart;
+    self.latitude = self._slowsLatitude + self._latDiff * self.acceleratorPedalPosition;
+    self.longitude = self._slowsLongitude + self._lngDiff * self.acceleratorPedalPosition;
+    self.odometer += self.vehicleSpeed / 3.6e+6;
     cb();
   }, 100);
 }
@@ -90,6 +121,8 @@ Automobile.prototype._stateFromVehicleSpeed = function(vehicleSpeed) {
   case 7:
     return 'seventh';
   case 8:
+    return 'eighth';
+  default:
     return 'eighth';
   }
 }
